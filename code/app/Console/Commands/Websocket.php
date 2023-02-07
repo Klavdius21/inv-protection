279a8f20ff523bca15ad93bb3d36a0877f73381c
Klavdius21 <mikhail@valyakin.ru>
2023-02-07 15:06:59 +0300

    add websocket command

---
+++ code/app/Console/Commands/Websocket.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+class websocket extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'websocket:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run websocket in server';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new \App\Helpers\Websocket()
+                )
+            ),
+            8080
+        );
+
+        $server->run();
+    }
+}
