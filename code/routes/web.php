85b3e76bdf2997fa0c77e975bdc0f03f4b15e90c
Klavdius21 <mikhail@valyakin.ru>
2023-02-07 16:22:04 +0300

    bug fix

--- code/routes/web.php
+++ code/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route:get('/', function (){
+Route::get('/', function (){
     return view('hello');
 });
 /*Route::get('/news', NewsController::class)->name('NewsHomepage');
